3b29b1330bde20a5f9ba65ef6461b17429854dfa
JaywalkingBanana <62420073+JaywalkingBanana@users.noreply.github.com>
2021-03-11 09:57:22 +0100

    Update fibonacci_sequence_recursively.C

--- fibonacci_sequence_recursively.C
+++ fibonacci_sequence_recursively.C
@@ -4,20 +4,22 @@ int fibonacci(int i);
 
 int main()
 {
-  for(int i = 1; i <= 10; i++)
+  for(int i = 0; i <= 10; i++)
   {
     printf("%d\t%d\n", i, fibonacci(i));
   }
 }
 
-int fibonacci(int i)
-{
-  if(i <= 2)
-  {
-    return 1;
+int fibonacci(int i){
+  if(i == 0){
+    return 0;
   }
-  else
-  {
-    return fibonacci(i - 1) + fibonacci(i - 2);
-  }
+  else{
+    if(i == 1 ){
+      return 1;
+    }
+    else{
+      return fibonacci(i - 1) + fibonacci(i - 2);
+    }
+  }  
 }
